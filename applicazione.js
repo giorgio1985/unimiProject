926feaa1a581984417c88c74a6cb6e7a78f61eb3
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2018-05-23 15:23:57 +0000

    Update applicazione.js

--- applicazione.js
+++ applicazione.js
@@ -1,52 +1,63 @@
+
+//mongodb+srv://<USERNAME>:<PASSWORD>@cluster1-flnc4.mongodb.net/test?retryWrites=true
+
 var express=require('express');
 var app=express();
-var fs=require('fs');
+
 var bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-//mongodb+srv://<USERNAME>:<PASSWORD>@cluster1-flnc4.mongodb.net/test?retryWrites=true
+
+var mongo=require('mongodb').MongoClient;
+var url='mongodb://127.0.0.1:27017/mongoDatabase';
+
+var fs=require('fs');
+var time=require('./time.js');
+
+var cripto=require('crypto-js');
 
 
-
-
-var MongoClient = require('mongodb').MongoClient;
-
-// replace the uri string with your connection string.
-var uri = "mongodb+srv://giorgio:giorgino85!@cluster1-flnc4.mongodb.net";
-MongoClient.connect(uri, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n'+err);
-   }
-   console.log('Connected. to mongodb..');
-   //const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   //client.close();
-
-
-
-
-});
 app.get('/index', function(req, res){
 fs.readFile(__dirname+'/'+'index.html', 'utf8', function(err, data){
 if (err) throw err;
 res.end(data);
-console.log('file is downloaded!');
+console.log('File is uploaded!: '+time.timeStamp());
 });
 });
 
-app.post('/applicazione', function(req, res){
-	var name=req.body.nome;
-	var surename=req.body.cognome;
-	var age=req.body.eta;
-	var password=req.body.password;
-	fs.readFile(__dirname+'/'+'applicazione.html','utf8' ,function(err, data){
-if(err) throw err;
-res.end('Your are send this informations: name: '+name+' surename: '+surename+'age: '+age+' And password: '+password);
-	console.log('informations send!');
+app.post('/inside', function(req, res){
+var name=req.body.nome;
+var pass=req.body.password;
+var email=req.body.email;
+var string=name+pass+email;
+var enc=cripto.MD5(string).toString();
+var passEncrypt=cripto.MD5(pass).toString();
+fs.readFile(__dirname+'/'+'inside.html', 'utf8', function(err, data){
+if (err) throw err;
+mongo.connect(url, function(err, db){
+	if (err) throw err;
+	var mydb=db.db('mongoDatabase');
+	var obj={_id: enc, Stamp: time.timeStamp(), Nome: name, Password: passEncrypt, Email: email};
+	mydb.collection('custums').insertOne(obj, function(err, data){
+		if (err) throw err;
+		res.end(JSON.stringify(obj));
 	});
+	mydb.collection('custums').find({}, {_id: 1}).toArray(function(err, data){
+        if (err) throw err;
+        res.end(JSON.stringify(obj));
+	});
+db.close();
+});
+
 });
 
 
 
+});
+//
+app.listen(8000);
 
-app.listen(7000);
+
+
+
+
