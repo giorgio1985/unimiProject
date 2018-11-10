df1c464ce4519b03eeed58c8db8cf5deeb6c85b2
giorgio1985 <g.adonoo@yahoo.it>
2018-11-11 00:48:52 +0100

    11-11-18 00:50

--- btcPrice.js
+++ btcPrice.js
@@ -1,19 +1,34 @@
+var express=require('express');
+var app=express();
+var fs = require('fs');
+var bodyP=require('body-parser');
+var request = require('request');
+const EventEmitter = require('events').EventEmitter;
+const  marketPrice = new EventEmitter;
+
 function btcStockPrice(){ 
-/*request({
+
+request({
   url: "http://blockchain.info/stats?format=json",
   json: true
 }, function(error, response, body){
   console.log('1 BTC = '+ body.market_price_usd + ' USD');
-res.end(body.market_price_usd);
-});*/
-
-var x = document.getElementsByClassName('btcTable')[0];
- var y = x.getElementsByTagName('p')[0];
-
-    y.innerHTML = "red";
-console.log('fatto');
-
+//res.end(body.market_price_usd);
+});
 }
 
+marketPrice.on('btcStockPrice', btcStockPrice);
+console.log('srfsrf')
+// ----------------------------------------------------------------------
+/*app.on('testEvent', function () {
+  return console.log('responded to testEvent');
+});
+
+app.get('/test', function (req, res) {
+  app.emit('testEvent');
+  return res.status(200).end();
+});
 
 //4
+
+*/
